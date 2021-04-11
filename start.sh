94be0f9d28d2dca5fb1a4fb71409490c892a87a4
outfrost <kotlet.bahn@gmail.com>
2021-04-11 18:46:05 +0200

    Update stream start script

--- start.sh
+++ start.sh
@@ -16,23 +16,21 @@ if [ ! -t 1 ]; then
 fi
 
 echo "Starting chat bot and system monitoring"
-tmux new -s 'stream_chat_bot' "$BOT_COMMAND" \; new-window 'livestream-sensors' \; detach
-sleep 0.1
+tmux new -s 'stream_chat_bot' "$BOT_COMMAND" \; detach
+sleep 0.2
 echo
-run ksysguard
 
 echo "Waiting up for chat bot's server to get ready"
 sleep 8
 
 echo "Starting Streamlabs recent events window"
-run firefox --ssb "https://streamlabs.com/dashboard/recent-events"
+run firefox --new-window "https://streamlabs.com/dashboard/recent-events"
 
-#echo "Starting Nightbot dashboard"
-#browser_command "https://beta.nightbot.tv/dashboard"
+echo "Starting Nightbot dashboard"
+run firefox --new-window "https://nightbot.tv/dashboard"
 
 echo "Starting Twitch Stream Manager"
 run firefox --new-window "https://dashboard.twitch.tv/u/$TWITCH_CHANNEL/stream-manager"
-#browser_command "https://dashboard.twitch.tv/u/$TWITCH_CHANNEL/stream-manager"
 
 echo "Starting OBS Studio"
 run obs
